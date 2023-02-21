53f4e2f7a3bb3bcc4e100ace064b58c49936a358
Katros <katros250@gmail.com>
2023-02-21 15:19:19 +0200

    Update blogValidation.js

--- learn-express/validation/blogValidation.js
+++ learn-express/validation/blogValidation.js
@@ -4,7 +4,7 @@ import Joi from "joi";
 
 const blogSchema = Joi.object({
     title: Joi.string().min(5).max(60).required(),
-	content: Joi.string().min(10).max(100).required(),
+	content: Joi.string().min(10).max(9000).required(),
 	Image: Joi.string()
 })
 
